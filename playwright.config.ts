a4d3928ea8c69dee95a7f51b4730b350e9239941
nguyenbatranvan <nguyenbatranvan96@gmail.com>
2024-05-02 21:19:47 +0700

    change config & workflow

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,5 +1,4 @@
 import {defineConfig, devices} from "@playwright/test";
-import {TestOptions} from "./src/playwright-utils";
 
 /**
  * Read environment variables from file.
@@ -11,73 +10,73 @@ import {TestOptions} from "./src/playwright-utils";
  * See https://playwright.dev/docs/test-configuration.
  */
 
-export default defineConfig<TestOptions>({
-  webServer: {
-    command: "npm run dev",
-    url: "http://localhost:3000",
-    reuseExistingServer: !process.env.CI,
-    stdout: "ignore",
-    stderr: "pipe"
-  },
+export default defineConfig({
+    webServer: {
+        command: "npm run dev",
+        url: "http://localhost:3000",
+        reuseExistingServer: !process.env.CI,
+        stdout: "ignore",
+        stderr: "pipe"
+    },
 
-  testDir: "packages/",
-  fullyParallel: true,
-  forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
-  reporter: "html",
-  use: {
-    baseURL: "http://localhost:3000",
-    headless: true,
-    ignoreHTTPSErrors: true,
-    screenshot: "only-on-failure",
-    video: "retain-on-failure",
-    extraHTTPHeaders: {},
-    trace: "on-first-retry"
-  },
+    testDir: "packages/",
+    fullyParallel: true,
+    forbidOnly: !!process.env.CI,
+    retries: process.env.CI ? 2 : 0,
+    workers: process.env.CI ? 1 : undefined,
+    reporter: "html",
+    use: {
+        baseURL: "http://localhost:3000",
+        headless: true,
+        ignoreHTTPSErrors: true,
+        screenshot: "only-on-failure",
+        video: "retain-on-failure",
+        extraHTTPHeaders: {},
+        trace: "on-first-retry"
+    },
 
-  /* Configure projects for major browsers */
-  projects: [
-    // {
-    //     name: "chromium",
-    //     use: {...devices["Desktop Chrome"]}
-    // },
-    //
-    // {
-    //     name: "firefox",
-    //     use: {...devices["Desktop Firefox"]}
-    // },
+    /* Configure projects for major browsers */
+    projects: [
+        {
+            name: "chromium",
+            use: {...devices["Desktop Chrome"]}
+        },
 
-    {
-      name: "webkit",
-      use: {...devices["Desktop Safari"]}
-    }
+        {
+            name: "firefox",
+            use: {...devices["Desktop Firefox"]}
+        },
 
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
+        {
+            name: "webkit",
+            use: {...devices["Desktop Safari"]}
+        }
 
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
-  ]
+        /* Test against mobile viewports. */
+        // {
+        //   name: 'Mobile Chrome',
+        //   use: { ...devices['Pixel 5'] },
+        // },
+        // {
+        //   name: 'Mobile Safari',
+        //   use: { ...devices['iPhone 12'] },
+        // },
 
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+        /* Test against branded browsers. */
+        // {
+        //   name: 'Microsoft Edge',
+        //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
+        // },
+        // {
+        //   name: 'Google Chrome',
+        //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+        // },
+    ]
+
+    /* Run your local dev server before starting the tests */
+    // webServer: {
+    //   command: 'npm run start',
+    //   url: 'http://127.0.0.1:3000',
+    //   reuseExistingServer: !process.env.CI,
+    // },
 });
