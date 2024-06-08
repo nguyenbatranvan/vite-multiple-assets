ddf39a99d997d384ae013a7742646f58e9bb613c
Farhan Sabran <animationfar@gmail.com>
2024-06-09 01:45:27 +0700

    clean: remore unnecessary comment, and add documentation of types.d.ts

--- packages/libs/build.ts
+++ packages/libs/build.ts
@@ -1,10 +1,10 @@
 import fs from "fs/promises";
 import type { NormalizedOutputOptions } from "rollup";
 import { isAbsolute, sep } from "path";
-import { join, relative, dirname, sep as posixSep } from "path/posix";
+import { join, resolve, relative, dirname, sep as posixSep } from "path/posix";
 import mm from "micromatch";
 import fg from "fast-glob";
-import { FDst, IAssets, IConfig, IFilesMapper, IViteResolvedConfig } from "./types";
+import type { FDst, IAssets, IConfig, IFilesMapper, IViteResolvedConfig } from "./types";
 
 // LINK https://nodejs.org/docs/latest/api/errors.html#common-system-errors
 export enum ErrorCode {
@@ -16,6 +16,14 @@ export enum ErrorCode {
 // TODO: test against "./**", "///**", "/**", "/{,foo}/**", "/{/,foo}/**"
 // TODO: test againts relative and absolute path
 // TODO: realtest
+
+/**
+ * Return basepath of relative path refering glob pattern
+ * - `/a/b/{\x01,c}/d/**` + `/a/b/c/d/efg.txt` -> `c/d/efg.txt`
+ * - `/a/b/c/d/**` + `/a/b/c/d/efg.txt` -> `efg.txt`
+ * 
+ * **NOTICE:** none of the results start with slash
+ */
 export function destinationResolver() {
     const list: Record<string, { 
         base: string;
@@ -42,15 +50,8 @@ export function destinationResolver() {
                     }),
                 };
             }
-            // REVIEW[epic-skeptical]: dstFile can not-match with the glob pattern
-            // REVIEW[epic-skeptical]: asset base can not-match with dstFile sliced base caused by uncertain 
-            // REVIEW[epic-skeptical]: begining path as "/", "../", "". Even in manual test, as user can fill
-            // REVIEW[epic-skeptical]: absolute and backwards, its base is right.
-            // FIXME[epic-skeptical]: asset base and dstFile sliced base should not-match caused by case-sensitivity OS.
-            // REVIEW: Filename like `["/etc/wwwroot/some/index.html", "C:/etc/wwwroot/dir2/index.html", "../../some/path/file.txt"]`
-            // REVIEW: is intended to lost its base path to `["some/index.html", "dir2/index.html", "file.txt"]`.
 
-            // if (opts.absolute && isAbsolute(dstFile) && !isAbsolute(list[asset].base)) dstFile = relative(opts.cwd, dstFile);
+            // FIXME[epic-skeptical]: asset base and dstFile sliced base should not-match caused by case-sensitivity OS.
             if(list[asset].isMatch(dstFile) && dstFile.slice(0, list[asset].base.length).toUpperCase() == list[asset].base.toUpperCase()) {
                 dstFile = dstFile.slice(list[asset].startGlob); // REVIEW: fixed because last slash before glob is not included in base
                 break;
@@ -65,7 +66,7 @@ export function destinationResolver() {
 export const internalDestination = destinationResolver();
 
 export async function getFiles (
-    files_: IAssets = [], // FIXME: user can fill `["/etc/wwwroot/**", "C:/etc/wwwroot/**", "../../some/path/**"]`
+    files_: IAssets = [],
     opts: IConfig = {}, 
     viteConfig: IViteResolvedConfig, 
     writeBundleOptions?: NormalizedOutputOptions
@@ -74,27 +75,19 @@ export async function getFiles (
     const files = await fg.glob(files_, {
         ignore: [],
         onlyFiles: true,
+        onlyDirectories: false,
         markDirectories: true,
         dot: true,
         ...opts,
-        absolute: false, // NOTE: MUST be Relative to match as close as possible to the pattern
+        absolute: false, // STUB: MUST be Relative to match as close as possible to the pattern
     });
     const mapper: IFilesMapper = {};
-    for (const filepath of files) { // FIXME: filepath can have `["/etc/wwwroot/index.html", "C:/etc/wwwroot/index.html", "../../some/path/file.txt"]`
+    for (const filepath of files) {
         // STUB: formerly to modify filepath to be relative and normalization, now leave as if for future modifying condition
-        // NOTE: baseFile MUST be Relative to make it easier to be sliced
-        // const baseFile = opts.absolute && isAbsolute(filepath) ? relative(opts.cwd, filepath) : filepath;
+        // STUB: baseFile MUST be Relative to make it easier to be sliced
         const baseFile = filepath;
 
-        let name: ReturnType<FDst> = baseFile; // FIXME: name as dstFile can have `["/etc/wwwroot/index.html", "C:/etc/wwwroot/index.html", "../../some/path/file.txt"]`
-
-        // STUB: formerly a normalizer path to remove root from basefile and dstfile
-        // STUB: as cause of not trusted on path.relative parse and filepath of glob
-        // NOTE: must have a bit of faith on plugin and core function used
-        // STUB: ./ ../ :/ :///
-        // const sepRG = /(\.|\:)+(\/|\\)+/i;
-        // if (dstFile.slice(0, 1) == sep) dstFile = dstFile.slice(1);
-        // if (dstFile.slice(0, 2) == "." + sep) dstFile = dstFile.slice(2);
+        let name: ReturnType<FDst> = baseFile; // STUB: as `baseFile`, this MUST be relative
 
         name = internalDestination.resolve({
             filepath, baseFile, opts, viteConfig, writeBundleOptions,
@@ -115,13 +108,12 @@ export async function getFiles (
             });
         if (_dstFile == false) continue;
         if (_dstFile == undefined) _dstFile = name as string;
-        _dstFile = _dstFile.replaceAll(sep, posixSep); // NOTE: force using posix
+        _dstFile = _dstFile.replaceAll(sep, posixSep); // STUB: force using posix
 
         // STUB: this intentionally left for future extentional purpose
-        // LINK /libs/build.ts#17
         name = _dstFile;
 
-        mapper[name as string] = join(opts.cwd!, filepath); // FIXME: filepath MUST Absolute
+        mapper[name as string] = resolve(opts.cwd!, filepath); // STUB: filepath MUST Absolute
     }
     return mapper;
 }
@@ -134,11 +126,10 @@ export async function buildMiddleWare(
 ) {
     const files = await getFiles(assets, opts, viteConfig, writeBundleOptions);
     for (const [_dstFile, filepath] of Object.entries(files)) {
-        // NOTE: "C:\\etc\\wwwroot", "\\etc\\wwwroot", "C:/etc/wwwroot", "/etc/wwwroot" is absolute and
-        // NOTE: would override default join. `dstFile` must be absolute.
-        const dstFile = isAbsolute(_dstFile) ? _dstFile : join(opts.__dst!, _dstFile);
+        // STUB: `dstFile` must be absolute.
+        const dstFile = isAbsolute(_dstFile) ? _dstFile : resolve(opts.__dst!, _dstFile);
 
-        // NOTE: using chain promise to handle further condition and more convenience
+        // STUB: using chain promise to handle further condition and more convenience
         await fs.mkdir(dirname(dstFile), { recursive: true })
             .then(() => fs.copyFile(filepath!, dstFile))
             .catch(async (reason) => { // FIXME: no .code in Error interface of nodejs. Need find the right interface.
--- packages/libs/types.d.ts
+++ packages/libs/types.d.ts
@@ -2,16 +2,46 @@ import type { Options } from "fast-glob";
 import type { PluginOption, ViteDevServer } from "vite";
 import type { NormalizedOutputOptions } from "rollup";
 
-// ANCHOR: extended to individual configuration on generating output
+/**
+ * Examples:
+ * - `/a/b/c/d/**` + `/a/b/c/d/efg.txt` -> `efg.txt`
+ * - Set basepath: `/a/b/{\x01,c}/d/**` + `/a/b/c/d/efg.txt` -> `c/d/efg.txt`
+ * ```ts
+ * DynamicPublicDirectory(["/a/b/c/d/**", "/a/b/{\x01,c}/d/**"])
+ * ```
+ * ANCHOR: extended to individual configuration on generating output\
+ * NOTE: for blank pattern on `{,filename}` assumed include everything by fast-glob. To fix it, use forbidden name or ascii (else than null).
+ *       For example `\x01`, `CON`, `NUL`, please see [this answer](https://stackoverflow.com/a/31976060).
+ */
 export type IAssets = string[]; // | string | (IConfigExtend & { assets: string | string[]; })[];
 export type IFilesMapper = Partial<Record<string, string>>; // STUB: { baseTransformedFilePath: toAbsolutePath }
 
 export type IMIME = Record<string, string>;
 
-// ANCHOR: when this function return false, it exclude the files.
-// ANCHOR: and if it return undefined, it would going with default.
-// NOTE: string must return either absolute path or relative path in 
-// NOTE: POSIX without beginning slash. It must also return forward slash.
+/**
+ * Return destination of files individually relative to output dir. 
+ * For example, if `viteConfig?.build.outDir` is set to `dist`, then
+ * - when `dst == undefined`, then file would be stored inside `viteConfig.build.outDir` or `writeBundleOptions?.dir` or `dist/`
+ * - when `dst == "./foo/bar"`, then file stored in `${viteConfig.build.outDir}/foo/bar/`
+ * - ```({dstFile}) => `.cache/${dstFile}` ```, then file stored in `${viteConfig.build.outDir}/.cache/`. NOTICE! no slash at beginning
+ * - This also can ```({dstFile}) => `../.static/${dstFile}` ``` which store file in `${viteConfig.root}/.static/`, the same level as `viteConfig.build.outDir`
+ * - `() => false`, then the file would be excluded. Good for complex ignoring files
+ * - , then file is manually named, and current built-in naming would be ignored. Good for complex use-case or need massive modification
+ * ```ts
+ * ({
+ *   {__dst, ...opts}, __files, // muttable
+ *   dstFile, filepath, baseFile, // useful params to use
+ * }) => {
+ *   __files["new-no/existing/file.png"] = "/home/me/Pictures/image.png"
+ *   return false;
+ * }
+ * ```
+ * - `() => undefined`, then it would going with default
+ * 
+ * **NOTE:** string must return either absolute path or relative path in POSIX without beginning slash. It must also return forward slash.\
+ * NOTE: the default actually put inside `__dst` not `viteConfig?.build.outDir` \
+ * NOTE: Please ALWAYS use posix slash, rather than win32 backslash
+ */
 export type FDst = (params: {
     dst: string;
     filepath: string;
