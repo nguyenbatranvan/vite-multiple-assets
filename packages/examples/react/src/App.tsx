72db58d46752f2755507810231b50dd27298f7e9
nguyenbatranvan <nguyenbatranvan96@gmail.com>
2024-10-05 10:40:01 +0700

    update example

--- packages/examples/react/src/App.tsx
+++ packages/examples/react/src/App.tsx
@@ -11,7 +11,7 @@ function App() {
         <>
             <div>
                 <a href="https://vitejs.dev" target="_blank">
-                    <img src={"/vite.svg"} className="logo" alt="Vite logo"/>
+                    <img src={"/shared-assets/vite.svg"} className="logo" alt="Vite logo"/>
                 </a>
                 <a href="https://react.dev" target="_blank">
                     <img aria-label={"logo-assets"} src={reactLogo} className="logo react" alt="React logo"/>
