73bfe990eadcdd8dbfa1731db79c82754426e3b2
nguyenbatranvan <nguyenbatranvan96@gmail.com>
2024-11-12 13:42:06 +0700

    add chokidar watch path

---
+++ packages/libs/src/utils/getTargetPathSymlink.ts
@@ -0,0 +1,11 @@
+import fs from "fs";
+import path from "node:path";
+
+export function getTargetPathSymlink(symlink: string) {
+    try {
+        const targetPath = fs.readlinkSync(symlink)
+        return path.resolve(path.dirname(symlink), targetPath)
+    } catch (e) {
+        return null
+    }
+}
---
+++ packages/libs/src/utils/index.ts
@@ -0,0 +1,1 @@
+export {getTargetPathSymlink} from "./getTargetPathSymlink"
