b36ab3bf3589e305c79de9058d38e01aa00395af
Van Nguyen <nguyenbatranvan96@gmail.com>
2024-11-22 14:21:42 +0700

    - Check failed format

--- scripts/share-run.js
+++ scripts/share-run.js
@@ -1,32 +1,36 @@
 const {spawn} = require("node:child_process");
-const fs = require("node:fs");
-const {join} = require("path");
 
 const args = process.argv.filter((item) => !item.includes("/"));
 const projectName = args[0];
 const opts = args.slice(1, args.length);
-// const filePath = join(process.cwd(), "apps/" + projectName + "/remote.json");
-
-// function fastReadFileSyncsToArray(filePath) {
-//     return JSON.parse(fs.readFileSync(filePath));
-// }
 
 function shareRun(mode) {
     runScripts(mode, " --filter=" + projectName);
 }
 
 function runScripts(mode, projects) {
+    let process;
     if (!args || !args.length) {
-        spawn("turbo", [mode, opts], {
+        process = spawn("turbo", [mode, opts], {
             shell: true,
             stdio: "inherit"
         });
     } else {
-        spawn("turbo", [mode, projects, opts], {
+        process = spawn("turbo", [mode, projects, opts], {
             shell: true,
             stdio: "inherit"
         });
     }
+    process.on('error', (code) => {
+            console.error(`Process error with code ${code}`);
+            process.exit(code);
+    });
+    process.on('close', (code) => {
+        if (code !== 0) {
+            console.error(`Process exited with code ${code}`);
+            process.exit(code);
+        }
+    });
 }
 
 module.exports = {
